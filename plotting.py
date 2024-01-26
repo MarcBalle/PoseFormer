04e6ae9287f209ec26e4f54b896a3f3e149021ff
MarcBalle <balle.marc97@gmail.com>
2024-01-26 15:59:10 +0100

    added 3d plotting

--- plotting.py
+++ plotting.py
@@ -8,7 +8,11 @@ Second for loop plots the skeleton following the Human3.6M convention.
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.load("drive&act_keypoints_notnorm.npz")["data"][0]
+plot_scatter = False
+plot_skeleton = False
+plot_3dskeleton = True
+
+data = np.load("prediction.npz")["data"][0]
 fig, axs = plt.subplots(3, 6)
 fig.set_figheight(18)
 fig.set_figwidth(18)
@@ -32,19 +36,38 @@ skeleton = [
     [15, 16],
 ]
 
-for i, ax in enumerate(axs.reshape(-1)):
-    if i < data.shape[0]:
-        ax.set_xlim(0, 960)
-        ax.set_ylim(-540, 0)
-        ax.scatter(data[0 : i + 1, 0], -data[0 : i + 1, 1], s=10)
+if plot_scatter:
+    for i, ax in enumerate(axs.reshape(-1)):
+        if i < data.shape[0]:
+            ax.set_xlim(0, 960)
+            ax.set_ylim(0, 540)
+            ax.scatter(data[0 : i + 1, 0], data[0 : i + 1, 1], s=10)
 
-plt.savefig("scatter_keypoints_notnorm.png")
-plt.clf()
+    plt.savefig("scatter_keypoints_notnorm.png")
+    plt.clf()
 
-fig = plt.figure()
-for joint in skeleton:
-    plt.scatter(data[:, 0], -data[:, 1])
-    plt.plot([data[joint[0], 0], data[joint[1], 0]], [-data[joint[0], 1], -data[joint[1], 1]])
+if plot_3dskeleton:
+    fig = plt.figure()
+    for joint in skeleton:
+        plt.scatter(data[:, 0], data[:, 1])
+        plt.plot(
+            [data[joint[0], 0], data[joint[1], 0]],
+            [data[joint[0], 1], data[joint[1], 1]],
+        )
+    plt.show()
 
+    plt.savefig("skeleton.png")
 
-plt.savefig("skeleton.png")
+if plot_3dskeleton:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    for joint in skeleton:
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2])
+        ax.plot(
+            [data[joint[0], 0], data[joint[1], 0]],
+            [data[joint[0], 1], data[joint[1], 1]],
+            zs=[data[joint[0], 2], data[joint[1], 2]],
+        )
+    plt.show()
+
+    plt.savefig("3dskeleton.png")
